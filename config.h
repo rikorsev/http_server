31ef4d3256117a3b22766c5b2d6de149cf2d988f
Alexandr Kolosov <rikorsev@gmail.com>
2022-09-12 01:58:47 +0300

    Add some doxigen to config and log modules

--- config.h
+++ config.h
@@ -1,10 +1,25 @@
+/**
+ * @file config.h
+ * @brief Provide access the solution configuration options
+ * 
+ **/
+
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+/** Enable support of keep-alive feature */
 #define CONFIG_KEEPALIVE_ENABLE 0
-#define CONFIG_KEEPALIVE_TIMEOUT_SEC 60
+
+/** Define timeout for keep-alive in seconds */
+#define CONFIG_KEEPALIVE_TIMEOUT_SEC 15
+
+/** Define size of buffer for input (from client to server) data in bytes */
 #define CONFIG_INPUT_BUFF_LEN 1024
+
+/** Define size of buffer for output (rom server to client) data in bytes */
 #define CONFIG_OUTPUT_BUFF_LEN 1024
+
+/** Define maxinum path size in HTTP request */
 #define CONFIG_MAX_PATH_SIZE 128
 
 #endif
--- log.h
+++ log.h
@@ -1,14 +1,34 @@
+/**
+ * @file log.h
+ * @brief Provide access to loggigh macroses
+ * 
+ **/
+
 #ifndef LOG_H_
 #define LOG_H_
 
 #include <stdio.h>
 #include <time.h>
 
+/**
+ * @brief Logout INFO message
+ * 
+ * @param msg[in] - formatted message to output
+ * @param ...[in] - additional arguments
+ * 
+ **/
 #define LOGINF(msg, ...) fprintf(stdout, "INF: [%08ld] ", time(NULL));\
                          fprintf(stdout, "%s: ", MODULE_NAME);\
                          fprintf(stdout, msg, ##__VA_ARGS__);\
                          fprintf(stdout, "\r\n")
 
+/**
+ * @brief Logout ERROR message
+ * 
+ * @param msg[in] - formatted message to output
+ * @param ...[in] - additional arguments
+ * 
+ **/
 #define LOGERR(msg, ...) fprintf(stderr, "ERR: [%08ld] ", time(NULL));\
                          fprintf(stderr, "%s: ", MODULE_NAME);\
                          fprintf(stderr, msg, ##__VA_ARGS__);\
